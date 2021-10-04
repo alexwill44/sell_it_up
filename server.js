32ee92b237872f617c75fd268121a987bd1a8312
alexwill44 <AlArvanitis@gmail.com>
2021-10-04 14:39:04 -0700

    url and params example

--- server.js
+++ server.js
@@ -1,34 +1,95 @@
 /* === External Modules === */
-const express =require('express');
+    const express =require('express');
 
 /* === Internal Modules === */
 
 /* === System Variables === */
-const app = express();
+    const app = express();
 
 /* === Routes === */
 
 // .get('url', call back function(request, responce){})
 
 // home route
-app.get('/', function (request, responce) {
-    // request is incoming
-    // responce is outgoing
-    
-    // .send(data)
-    responce.send('Hellow World');
-});
+    app.get('/', function (request, responce) {
+// request is incoming
+// responce is outgoing
 
-    // cats route
+// .send(data)
+    responce.send('Hellow World');
+    });
+
+// temp database
+    const products = [ 't-shirt', 'shoes', 'necklace', 'Baller wrist band'];
+
+// products 
+    app.get('/products', function(req, res){
+        res.send(products);
+    });
+
+/*   // first product 
+    app.get('/products/0', function(rec, res){
+        res.send(products[0]);
+    });
+
+//second product
+    app.get('/products/1', function(rec, res){
+        res.send(products[1]);
+    });
+
+//thrid product
+    app.get('/products/2', function(rec, res){
+        res.send(products[2]);
+    }); */
+
+// product show 
+
+// NOTE ORDER MATTERS 
+
+// product HELP
+    app.get('/products/help', function (req, res){
+        res.send('Help');
+    })
+   
+// url parameters
+    app.get('/products/:index', function (req, res){
+        console.log(req.params);
+        res.send(products[req.params.index]);
+    });
+
+
+// cats route
     app.get('/cats', function (request, responce){
-        responce.send('Charley')
+        responce.send('Charley');
     })
 
-    //dogs route
+//dogs route
     app.get('/dogs', function (request, responce){
-        responce.send('Milo')
+        responce.send('Milo');
+    });
+
+// server params 
+    app.get('/user/:id', (req, res) => {
+        res.send(req.params);
+    });
+
+// multiple paramsc - all params must be provided to hit the route 
+// add nums
+    app.get ('/add/:x/:y', function (req, res){
+    
+        const x = parseInt(req.params.x);
+        const y = parseInt(req.params.y);
+
+        res.send(`${+x+y}`);
+    });
+
+// Query params - are optional
+// ?q=cats&type=gif
+    app.get('/search', function (req, res){
+        res.send(req.query);
     });
 
 /* === Server Listener === */
-app.listen(4000, function (){
-    console.log('Listening for Client Requests on port 4000')});
+    app.listen(4000, function (){
+        console.log('Listening for Client Requests on port 4000')
+    });
