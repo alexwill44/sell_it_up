06f443ef088b38cd48e22ab1434a5e7888feb841
alexwill44 <AlArvanitis@gmail.com>
2021-10-04 12:35:19 -0700

    base server setup

---
+++ server.js
@@ -0,0 +1,35 @@
+/* === External Modules === */
+const express =require('express');
+
+
+/* === Internal Modules === */
+
+/* === System Variables === */
+const app = express();
+
+/* === Routes === */
+
+// .get('url', call back function(request, responce){})
+
+// home route
+app.get('/', function (request, responce) {
+    // request is incoming
+    // responce is outgoing
+    
+    // .send(data)
+    responce.send('Hellow World');
+});
+
+    // cats route
+    app.get('/cats', function (request, responce){
+        responce.send('Charley')
+    })
+
+    //dogs route
+    app.get('/dogs', function (request, responce){
+        responce.send('Milo')
+    });
+
+/* === Server Listener === */
+app.listen(4000, function (){
+    console.log('Listening for Client Requests on port 4000')});
