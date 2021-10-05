7cbcafe99d16ff9de5873ba5bb338fc2c8e0d706
alexwill44 <AlArvanitis@gmail.com>
2021-10-04 20:45:57 -0700

    added 404 error

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 /* === External Modules === */
     const express =require('express');
+const product = require('./models/Product');
 
 /* === Internal Modules === */
 
@@ -7,6 +8,15 @@ const Product = require('./models/Product');
 
 /* === System Variables === */
     const app = express();
+
+/* === System Configuration === */
+
+// tell express that we will be using ejs 
+    app.set('view engine', 'ejs');
+
+// make public directory availible
+// and our css and js will now be reachable
+    app.use(express.static('public'))
 
 /* === Routes === */
 
@@ -23,19 +33,40 @@ const Product = require('./models/Product');
 
 //  index 
     app.get('/products', function(req, res){
-        res.send(Product.find);
+      //  res.send(Product.find());
+      // takes in the ejs file to turn into html
+      // takes in the context of what data should be used 
+      // .render('filename', data)
+      const context = {
+          students: 'Alex',
+          products: Product.find(),
+      };
+
+      res.render('index', context);
     });
 
 
-//  show 
-    app.get('/products/:id', function (req, res){
-       Product.findById(req.params.id, function ( error, product) {
-           if (error){
-              return res.send(error)
-           }{
-               res.send(product);
-           }
-       });
+//  show ---- FIXME
+    app.get('/products/:id', function (req, res, next) {
+        Product.findById(req.params.id, function (error, product) {
+            if (error) {_
+                req.error = error;
+                return next();
+            }
+
+               const context = {
+                   product, 
+                };
+                
+            res.render('show', context );
+        
+        });
+    });
+    
+
+    app.get('/*', function(req, res) {
+        const context = {error: req.error };
+        res.render('404', context);
     });
 
 /* === Server Listener === */
